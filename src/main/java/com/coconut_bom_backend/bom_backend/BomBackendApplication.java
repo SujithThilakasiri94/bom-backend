d36082593b82c9f45e9a5aef0a2bf1184960f6b8
Sujith Thilakasiri <sujiththilakasiri@outlook.com>
2024-04-29 16:17:40 +0530

    initial commit

---
+++ src/main/java/com/coconut_bom_backend/bom_backend/BomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coconut_bom_backend.bom_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coconut_bom_backend/bom_backend/BomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coconut_bom_backend.bom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
